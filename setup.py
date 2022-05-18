3b9ff0e09083690eb09c39adf9efc0d07897921a
MattD <matthewdaunt@obsolete.local>
2022-05-18 15:39:48 -0400

    setup files and hardcoded version

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
 from setuptools import setup
 import os
-VERSION_TEMPLATE = """
-# Note that we need to fall back to the hard-coded version if either
-# setuptools_scm can't be imported or setuptools_scm can't determine the
-# version, so we catch the generic 'Exception'.
-version = 1.0
-""".lstrip()
 
-setup(use_scm_version={'write_to': os.path.join('src', 'version.py'),
-                       'write_to_template': VERSION_TEMPLATE})
+setup(version=1.0)
