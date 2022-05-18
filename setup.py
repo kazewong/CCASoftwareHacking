5cfb7794d8163fc77a6f607d987be3d28da0290f
MattD <matthewdaunt@obsolete.local>
2022-05-18 15:26:37 -0400

    setup files added

--- setup.py
+++ setup.py
@@ -4,11 +4,7 @@ VERSION_TEMPLATE = """
 # Note that we need to fall back to the hard-coded version if either
 # setuptools_scm can't be imported or setuptools_scm can't determine the
 # version, so we catch the generic 'Exception'.
-try:
-    from setuptools_scm import get_version
-    version = get_version(root='..', relative_to=__file__)
-except Exception:
-    version = '{version}'
+version = 1.0
 """.lstrip()
 
 setup(use_scm_version={'write_to': os.path.join('src', 'version.py'),
